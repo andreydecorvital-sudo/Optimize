2dc2a6bfe609877fad6645f24a9aabd39c71569a
andreydecorvital-sudo <andreydecorvital@gmail.com>
2026-08-01 13:06:51 -0300

    Fail closed on standalone timer tweak

--- SysManager/SysManager/Views/TimerResolutionView.xaml.cs
+++ SysManager/SysManager/Views/TimerResolutionView.xaml.cs
@@ -2,11 +2,18 @@
 // Author: laurentiu021 · https://github.com/laurentiu021/SystemManager
 // License: MIT
 
+using System.Windows;
 using System.Windows.Controls;
+using SysManager.Helpers;
 
 namespace SysManager.Views;
 
 public partial class TimerResolutionView : UserControl
 {
-    public TimerResolutionView() => InitializeComponent();
+    public TimerResolutionView()
+    {
+        InitializeComponent();
+        Loaded += (_, _) => LegacyOptimizationLock.Apply(this,
+            "O ajuste isolado de resolução do temporizador está bloqueado. Use o Perfil para jogos, que aplica e reverte esse ajuste somente durante a sessão quando for compatível.");
+    }
 }
