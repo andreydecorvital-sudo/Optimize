5fb8b01d99aec952a7cd97be3171cfa8e8c3e13d
andreydecorvital-sudo <andreydecorvital@gmail.com>
2026-08-01 12:51:24 -0300

    Translate interpolated runtime strings to pt-BR

--- SysManager/SysManager/Services/PtBrLocalizationService.cs
+++ SysManager/SysManager/Services/PtBrLocalizationService.cs
@@ -40,6 +40,8 @@ public static class PtBrLocalizationService
             return migrated;
         if (PtBrMigrationCatalog4.TryTranslate(text, out migrated))
             return migrated;
+        if (PtBrRuntimePatternCatalog.TryTranslate(text, out migrated))
+            return migrated;
         return PtBrTranslationCatalog.Translate(text);
     }
 
