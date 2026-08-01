5d0cb479d7bcf2a4d7bdd6a8b93a7d46023c4ed3
andreydecorvital-sudo <andreydecorvital@gmail.com>
2026-08-01 13:07:27 -0300

    Fail closed on ungated tweaks hub

--- SysManager/SysManager/Views/TweaksHubView.xaml.cs
+++ SysManager/SysManager/Views/TweaksHubView.xaml.cs
@@ -2,11 +2,18 @@
 // Author: laurentiu021 · https://github.com/laurentiu021/SystemManager
 // License: MIT
 
+using System.Windows;
 using System.Windows.Controls;
+using SysManager.Helpers;
 
 namespace SysManager.Views;
 
 public partial class TweaksHubView : UserControl
 {
-    public TweaksHubView() => InitializeComponent();
+    public TweaksHubView()
+    {
+        InitializeComponent();
+        Loaded += (_, _) => LegacyOptimizationLock.Apply(this,
+            "A Central de ajustes herdada está em modo somente leitura. Cada tweak será liberado individualmente quando tiver compatibilidade, risco, backup e reversão auditados pelo Optimize.");
+    }
 }
