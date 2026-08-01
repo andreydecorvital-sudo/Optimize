40b8ce96cc04460180adf9eb2360e97972331ea7
andreydecorvital-sudo <andreydecorvital@gmail.com>
2026-08-01 13:07:04 -0300

    Fail closed on standalone CPU affinity tweak

--- SysManager/SysManager/Views/CpuAffinityView.xaml.cs
+++ SysManager/SysManager/Views/CpuAffinityView.xaml.cs
@@ -2,11 +2,18 @@
 // Author: laurentiu021 · https://github.com/laurentiu021/SystemManager
 // License: MIT
 
+using System.Windows;
 using System.Windows.Controls;
+using SysManager.Helpers;
 
 namespace SysManager.Views;
 
 public partial class CpuAffinityView : UserControl
 {
-    public CpuAffinityView() => InitializeComponent();
+    public CpuAffinityView()
+    {
+        InitializeComponent();
+        Loaded += (_, _) => LegacyOptimizationLock.Apply(this,
+            "A afinidade manual de CPU está bloqueada até a topologia deste processador ser validada. Use o Perfil para jogos para ajustes temporários, por processo e reversíveis.");
+    }
 }
