a3ecc2ac2858bb15a34e18cb6769a5818152cce5
andreydecorvital-sudo <andreydecorvital@gmail.com>
2026-08-01 12:39:53 -0300

    Load second pt-BR migration batch at runtime

--- SysManager/SysManager/Services/PtBrLocalizationService.cs
+++ SysManager/SysManager/Services/PtBrLocalizationService.cs
@@ -34,6 +34,8 @@ public static class PtBrLocalizationService
     {
         if (PtBrMigrationCatalog.TryTranslate(text, out var migrated))
             return migrated;
+        if (PtBrMigrationCatalog2.TryTranslate(text, out migrated))
+            return migrated;
         return PtBrTranslationCatalog.Translate(text);
     }
 
